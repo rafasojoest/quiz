9de054c8356ad9a849213e5dc2760c5729d695b9
rafasojoest <rafasojoest@gmail.com>
2015-07-17 10:01:13 +0200

    primera pregunta
    primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 
 var router = express.Router();
-
+var quizController = require('../controllers/quiz_controller');
 
 
 // Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: []});
 });
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
+
 
 module.exports = router;
